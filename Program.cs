7943c223d1b3889b2c38b950d2e2ef424467b8ce
gigaproxy <66910177+gigaproxy@users.noreply.github.com>
2021-10-26 17:04:46 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CSharpLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = 5;
+            int count = 0;
+            Console.WriteLine("Введите пятизначное число:");
+            string numberString = Console.ReadLine();
+            Console.WriteLine($"Ваше число: {numberString}");
+            int number = int.Parse(numberString);
+            int i = number;
+            foreach (char c in i.ToString())
+            {
+                count = count + 1;
+            }
+
+            if (count > 5)
+            {
+                Console.Write("Многовато");
+            }
+            if (count < 5)
+            {
+                Console.Write("Маловато");
+            }
+            if (count == 5)
+            {
+                foreach (char c in i.ToString())
+                {
+                    Console.Write(c + "\n");
+                }
+            }
+        }
+    }
+}
